64bc6589e94e6ccf40923dc19d5d0b457fed9fa1
cseswapon <cseswaponsaha@gmail.com>
2024-06-12 00:12:08 +0600

    Some code change with datatype file

---
+++ string.py
@@ -0,0 +1,27 @@
+a = """
+    Hello Bangladesh...!!!
+    How are you...?
+""";
+print(a);
+
+b = "Hello, World!";
+print(b[0]);
+
+print("B Length",len(b),b);
+
+for x in 'banana':
+    print(x);
+
+for s in 'Swapon Saha':
+    print(s);
+txt = "The best things in life are free!"
+print("free" not in txt);
+
+# slice string
+
+b = "Hello, World!";
+print(b[2:6])
+name = "Swapon Kumar Saha";
+# print("Saha" in name)
+print(name[9:len(name)])
+print(name[:7])
